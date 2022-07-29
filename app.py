9b159f39c458aeae4212aac8e08c826615a4deac
sunithvs <sunithvs2002@gmail.com>
2022-07-29 21:31:24 +0530

    init

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 from os import system as execute
 
 app = Flask(__name__)
@@ -9,7 +9,7 @@ app.debug = False
 def hello_world():  # put application's code here
     execute("cd ~/public/ ")
     execute("git pull ")
-    return "All files are up to date"
+    return jsonify(detail='success')
 
 
 if __name__ == '__main__':
